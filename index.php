23c23f880c6cb6c055f02a55c1e255a5e51c8676
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-11-07 15:55:46 -0500

    update

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>CS: Clothing Store</title>
+    <?php include('./includes/mysql.php'); ?>
+  </head>
+  <body>
+
+  </body>
+</html>
