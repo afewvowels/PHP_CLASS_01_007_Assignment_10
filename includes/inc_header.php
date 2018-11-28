b712329a9ab840e4c417d8b56988d6a0082a8d9d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-11-28 15:20:23 -0500

    update

--- includes/inc_header.php
+++
@@ -1,9 +0,0 @@
-<section id='nav_section'>
-  <section id='logo'>
-    <a href='display_errors.php'><h2>CS</h2></a>
-    <h1>The Clothing Store</h1>
-  </section>
-  <?php
-    create_login_header_field();
-  ?>
-</section>
